ac5221970773c7b2344ebacccd6b06148e2fe17c
IrynaOvdii <irina.ovdij@gmail.com>
2018-01-26 11:09:27 +0100

    Add API artists show

--- app/controllers/api/artists_controller.rb
+++ app/controllers/api/artists_controller.rb
@@ -1,7 +1,23 @@
 class Api::ArtistsController < ApplicationController
+  # before_action: :set_artist, only: [:show]
   def index
     render status: 200, json: {
       artists: Artist.all
     }.to_json
+  end
+
+  def show
+    artist = Artist.find(params[:id])
+    render status: 200, json: {
+      artist: artist
+    }.to_json
+  end
+
+  def artist_params
+    params.require(:artist).permit(:name, :image_url)
+  end
+
+  def image_params
+    params[:images].present? ? params.require(:images) : []
   end
 end
