6751441fd682e20a71ec61620255a84a27f7327e
IrynaOvdii <irina.ovdij@gmail.com>
2018-01-26 09:30:58 +0100

    Add API artists index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
   # For details on thRails.application.routes.draw do
   root 'artists#index'
-  resources :artists do
-    resources :songs
+
+    resources :artists do
+      resources :songs
+    end
+      resources :photos, only: [:destroy]
+
+    namespace :api do
+      resources :artists do
+        resources :songs
+      end
+        resources :photos, only: [:destroy]
+    end
   end
-    resources :photos, only: [:destroy]
-end
