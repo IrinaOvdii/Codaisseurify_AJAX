9c9f108670d9359c193a31a00fac7d0ec110692c
IrynaOvdii <irina.ovdij@gmail.com>
2018-01-27 11:32:06 +0100

    Add API, Serializer

--- app/controllers/api/artists_controller.rb
+++ app/controllers/api/artists_controller.rb
@@ -1,17 +1,25 @@
 class Api::ArtistsController < ApplicationController
-  # before_action: :set_artist, only: [:show]
+  skip_before_action :verify_authenticity_token
+  #before_action :set_artist, only: [:show]
+
   def index
-    render status: 200, json: {
-      artists: Artist.all
-    }.to_json
+    artists = Artist.all
+    render status: 200, json: artists
   end
 
   def show
     artist = Artist.find(params[:id])
-    render status: 200, json: {
-      artist: artist
-    }.to_json
+    songs = artist.songs
+    render status: 200, json: artist.songs
   end
+
+
+
+
+  # def set_artist
+  #   artist = Artist.find(params[:id])
+  #   render status: 200, json: artist
+  # end
 
   def artist_params
     params.require(:artist).permit(:name, :image_url)
@@ -20,4 +28,5 @@ class Api::ArtistsController < ApplicationController
   def image_params
     params[:images].present? ? params.require(:images) : []
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
       end
         resources :photos, only: [:destroy]
     end
+
   end
